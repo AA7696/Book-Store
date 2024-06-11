21ff7991ac212bfe43a8046c97a22973c0cf5376
Aachman <achmanagarwal7696@gmail.com>
2024-06-11 13:18:26 +0530

    Book Store updated

--- FrontEnd/src/About/About.jsx
+++ FrontEnd/src/About/About.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const About = () => {
   return (
     <>
-      
+      <div className="about w-screen h-screen flex flex-col justify-center items-center gap-12">
+        <h1 className=' text-blue-500'>About Us</h1>
+        <p className=' w-[55%] text-black  text-xl  '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam odit ipsum laboriosam similique, sequi perspiciatis exercitationem officiis repellendus modi aliquid accusantium at aut nesciunt, dignissimos non amet quisquam soluta doloribus. Rerum voluptatem harum assumenda cumque? Nostrum odio perspiciatis fugiat ipsum!</p>
+
+      </div>
     </>
   )
 }
